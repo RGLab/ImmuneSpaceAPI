aac3fe9e485378e9fa833b00919ea5df191f19bb
Evan Henrich <ehenrich@fredhutch.org>
2020-05-19 13:22:40 -0700

    update loadLocalFile to look for env var when testing

--- R/utils.R
+++ R/utils.R
@@ -3,10 +3,21 @@
 #' @param fileName file name sub-string to use for search
 #' @export
 loadLocalFile <- function(fileName){
+  # check for localPath if doing local testing
+  localPath <- tryCatch({
+    get("localPath")
+    }, error = function(e){
+      return("")
+    })
+
+  pathToUse <- ifelse(nchar(localPath) > 0,
+                      localPath,
+                      "/app/")
+
   # pull most recent file in case any old objects persist
-  fl <- grep(paste0("\\d{4}-\\d{2}-\\d{2}_", fileName),
-             list.files(path = "/app/"),
+  fls <- grep(paste0("\\d{4}-\\d{2}-\\d{2}_", fileName),
+             list.files(path = pathToUse),
              value = TRUE)
-  fl <- sort(fl, decreasing = TRUE)[[1]]
-  ret <- readRDS(paste0("/app/", fl))
+  fl <- sort(fls, decreasing = TRUE)[[1]]
+  ret <- readRDS(paste0(pathToUse, fl))
 }
