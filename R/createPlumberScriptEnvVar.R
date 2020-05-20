167d4dc6cdee24334b39fcb0a2f5c861d66be293
Evan Henrich <ehenrich@fredhutch.org>
2020-05-20 16:12:41 -0700

    change to have internal fn copy script for plumber

--- R/createPlumberScriptEnvVar.R
+++ R/createPlumberScriptEnvVar.R
@@ -1,12 +1,10 @@
 # Wrapper for easy use from Docker
 # Not expecting library to have been loaded before call to `startAPI()`
 
-#' Wrapper function for starting plumber service from within Docker
+#' Wrapper function for copying plumber script to specified location
 #'
 #' @export
-createPlumberScriptEnvVar <- function(filePath){
-  pathToScript <- file.path(system.file(package = "ImmuneSpaceLabKeyAPI"),
-                            "main.R")
-  file.create(filePath)
-  writeLines(pathToScript, filePath)
+copyPlumberScriptToLocation <- function(outputPath){
+  pathToScript <- file.path(system.file(package = "ImmuneSpaceLabKeyAPI"), "main.R")
+  file.copy(pathToScript, outputPath)
 }
